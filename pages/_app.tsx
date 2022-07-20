dfcc2d76762c51a6504a84dea5640d6f3102ffcf
tahmdev <qwertiimusic@gmail.com>
2022-07-20 13:47:16 +0200

    Update components folder structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Navbar } from "../components/navbar";
+import { Navbar } from "../components/nav/navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
