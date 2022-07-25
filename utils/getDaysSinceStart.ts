93ca483d18aea797b82b2bfb0423ba1165c7dc4a
tahmdev <qwertiimusic@gmail.com>
2022-07-25 14:43:26 +0200

    Set reset time to 00:00 JST

--- utils/getDaysSinceStart.ts
+++ utils/getDaysSinceStart.ts
@@ -1,6 +1,6 @@
 function getDaysSinceStart() {
   const now = Math.floor(new Date().getTime() / 1000);
-  const start = now - 1658667900;
+  const start = now - 1658588400;
   const daysSinceStart = start / 86400;
 
   return Math.floor(daysSinceStart);
