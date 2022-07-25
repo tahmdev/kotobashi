dca54e68b87a8b3d35760ec20b76717515fd346b
tahmdev <qwertiimusic@gmail.com>
2022-07-25 04:54:02 +0200

    Improve readability

--- utils/getWordChain.ts
+++ utils/getWordChain.ts
@@ -1,28 +1,29 @@
-function getWordChain(words: string[], length: number, start?: number) {
-  if (!start) start = randomIndex(words);
-  let result = [words[start]];
+function getWordChain(words: string[], length: number) {
+  let result: string[] = [];
   while (result.length < length && words.length) {
     if (!result.length) {
       result = [words[randomIndex(words)]];
     }
 
-    const chainableWords = words.filter((el) =>
-      el.startsWith(result[result.length - 1].slice(-1))
-    );
+    const chainableWords = getChainableWords(result[result.length - 1], words);
 
     if (chainableWords.length) {
       const i = randomIndex(chainableWords);
       result = [...result, chainableWords[i]];
       words = words.filter((el) => el !== chainableWords[i]);
     } else {
-      const removed = result.pop();
-      words = words.filter((el) => el !== removed);
+      result.pop();
     }
   }
   return result;
 }
-export default getWordChain;
 
 function randomIndex(arr: any[]) {
   return Math.floor(Math.random() * arr.length);
 }
+
+function getChainableWords(start: string, wordList: string[]) {
+  return wordList.filter((el) => el.startsWith(start.slice(-1)));
+}
+
+export default getWordChain;
